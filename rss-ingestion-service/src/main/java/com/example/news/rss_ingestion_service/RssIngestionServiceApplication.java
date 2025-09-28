8f949c4c301cc6460cfb6f2deacbdc5e949c5baf
AnuragSharma122 <2020kuec2015@iiitkota.ac.in>
2025-09-28 19:47:45 +0530

    Setup multi-service project structure

---
+++ rss-ingestion-service/src/main/java/com/example/news/rss_ingestion_service/RssIngestionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.news.rss_ingestion_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RssIngestionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RssIngestionServiceApplication.class, args);
+	}
+
+}
---
+++ rss-ingestion-service/src/test/java/com/example/news/rss_ingestion_service/RssIngestionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.news.rss_ingestion_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RssIngestionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

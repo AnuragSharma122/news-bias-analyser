5eb9665b9fbe93d4c059c470d338ab8d729323c0
Anurag Sharma <anuragsharma@192.168.1.69>
2025-10-11 01:37:20 +0530

    Fixed kafka ingestion issues.\n 1. Updated key serializer to StringSerializer\n 2. Updated json serializer to JsonSerializer

--- rss-ingestion-service/src/main/java/com/example/news/rss_ingestion_service/kafka/KafkaIngestionService.java
+++ rss-ingestion-service/src/main/java/com/example/news/rss_ingestion_service/kafka/KafkaIngestionService.java
@@ -2,18 +2,19 @@ package com.example.news.rss_ingestion_service.kafka;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 @Slf4j
-@AllArgsConstructor
 public class KafkaIngestionService {
 
+    @Autowired
     private KafkaTemplate<String, ArticleKafkaMessage> kafkaTemplate;
 
-    @Value("${app.kafka.topic.article-ingested}")
+    @Value("${app.kafka.topic.article}")
     private String topic;
 
     public void sendMessage(ArticleKafkaMessage message){
